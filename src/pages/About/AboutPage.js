7b30b30fd812f4233f3d00315936f8c68567539c
Ping Cheng <ping.cheng@aroflo.com>
2021-02-12 23:52:16 +1100

    add empty about page

---
+++ src/pages/About/AboutPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class AboutPage extends Component {
+
+	render() {
+		return (
+			<div className="container-body">
+				Hello
+			</div>
+		)
+	}
+
+}
