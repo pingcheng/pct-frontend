47cacc99c1468d3bb93e7be619b9cab09d13435b
Ping Cheng <ping.che@hotmail.com>
2021-02-21 23:52:24 +1100

    update identifier

--- src/pages/Posts/PostDetailPage.js
+++ src/pages/Posts/PostDetailPage.js
@@ -86,8 +86,8 @@ export default class PostDetailPage extends Component {
 					shortname="pingchengtech"
 					config={
 						{
-							url: window.location.origin,
-							identifier: this.props.location.pathname,
+							url: `${window.location.origin}/${this.props.location.pathname}`,
+							identifier: `post/${this.props.match.params.slug}`,
 							title: this.state.title
 						}
 					}
