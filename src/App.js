1f3caf1e2e03945b036d10aa51d69a73c7c79e0a
Ping Cheng <ping.che@hotmail.com>
2021-04-17 00:34:27 +1000

    add sentry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import PageRouter from "./PageRouter";
+import "./injections/Sentry";
 
 function App() {
   return (
---
+++ src/injections/Sentry.js
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [
+        new Integrations.BrowserTracing()
+    ],
+
+    tracesSampleRate: 1.0,
+});
