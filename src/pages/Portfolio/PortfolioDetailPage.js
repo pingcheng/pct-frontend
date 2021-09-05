5db0ee7472a17a153e7cf6497dc722bca5fa9c4b
Ping Cheng <ping.che@hotmail.com>
2021-09-05 14:23:56 +1000

    fix indents

--- src/pages/Portfolio/PortfolioDetailPage.js
+++ src/pages/Portfolio/PortfolioDetailPage.js
@@ -38,78 +38,78 @@ export default class PortfolioDetailPage extends Component {
 						<NotFoundPage />
 					) : (
 						<div>
-						<div className="container-body">
-							<div>
-								<Link to="/portfolio">
-									<IoMdArrowRoundBack/> Go back
-								</Link>
-							</div>
-
-							<Heading title={this.state.portfolio.name} align="center"/>
-
-							<div className="flex flex-wrap">
-								<div className="w-full md:w-1/2 flex justify-center portfolios-center">
-									<PortfolioCard name={this.state.portfolio.name}
-												   description={this.state.portfolio.shortDescription}
-												   image={this.state.portfolio.coverImage}/>
-								</div>
-
-								<div className="w-full md:w-1/2">
-									<SimpleRowData label="project">
-										{this.state.portfolio.name}
-									</SimpleRowData>
-
-									<SimpleRowData label="project url">
-										{this.state.portfolio.url === null ? "-" : (
-											<a href={this.state.portfolio.url}>{this.state.portfolio.url}</a>
-										)}
-									</SimpleRowData>
-
-									<SimpleRowData label="description">
-										{this.state.portfolio.longDescription}
-									</SimpleRowData>
-
-									<SimpleRowData label="workplace">
-										{this.state.portfolio.workplace}
-									</SimpleRowData>
-
-									<SimpleRowData label="project role">
-										{this.state.portfolio.projectRole}
-									</SimpleRowData>
-
-									<SimpleRowData label="role description">
-										{this.state.portfolio.roleDescription.map((line, index) => (
-											<div key={index}>{line}</div>
-										))}
-									</SimpleRowData>
-
-									<SimpleRowData label="team members">
-										{this.state.portfolio.members.map((member, index) => (
-											<div key={index}>{member}</div>
-										))}
-									</SimpleRowData>
-								</div>
-							</div>
-						</div>
-
-						<div className="w-full bg-black py-10">
 							<div className="container-body">
-								<Heading title="Screenshots" align="center" className="text-white"/>
+								<div>
+									<Link to="/portfolio">
+										<IoMdArrowRoundBack/> Go back
+									</Link>
+								</div>
 
-								<div className="portfolio-screenshots">
-									{
-										this.state.portfolio.screenshots.map((image, index) => (
-											<img
-												key={index}
-												src={image}
-												alt="screenshot"
-											/>
-										))
-									}
+								<Heading title={this.state.portfolio.name} align="center"/>
+
+								<div className="flex flex-wrap">
+									<div className="w-full md:w-1/2 flex justify-center portfolios-center">
+										<PortfolioCard name={this.state.portfolio.name}
+													   description={this.state.portfolio.shortDescription}
+													   image={this.state.portfolio.coverImage}/>
+									</div>
+
+									<div className="w-full md:w-1/2">
+										<SimpleRowData label="project">
+											{this.state.portfolio.name}
+										</SimpleRowData>
+
+										<SimpleRowData label="project url">
+											{this.state.portfolio.url === null ? "-" : (
+												<a href={this.state.portfolio.url}>{this.state.portfolio.url}</a>
+											)}
+										</SimpleRowData>
+
+										<SimpleRowData label="description">
+											{this.state.portfolio.longDescription}
+										</SimpleRowData>
+
+										<SimpleRowData label="workplace">
+											{this.state.portfolio.workplace}
+										</SimpleRowData>
+
+										<SimpleRowData label="project role">
+											{this.state.portfolio.projectRole}
+										</SimpleRowData>
+
+										<SimpleRowData label="role description">
+											{this.state.portfolio.roleDescription.map((line, index) => (
+												<div key={index}>{line}</div>
+											))}
+										</SimpleRowData>
+
+										<SimpleRowData label="team members">
+											{this.state.portfolio.members.map((member, index) => (
+												<div key={index}>{member}</div>
+											))}
+										</SimpleRowData>
+									</div>
+								</div>
+							</div>
+
+							<div className="w-full bg-black py-10">
+								<div className="container-body">
+									<Heading title="Screenshots" align="center" className="text-white"/>
+
+									<div className="portfolio-screenshots">
+										{
+											this.state.portfolio.screenshots.map((image, index) => (
+												<img
+													key={index}
+													src={image}
+													alt="screenshot"
+												/>
+											))
+										}
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 					)
 				}
 			</div>
