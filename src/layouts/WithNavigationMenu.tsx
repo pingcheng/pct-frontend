964fa13d435524455098d628b600a25e93ab4adf
Ping Cheng <neroc7@gmail.com>
2022-03-05 21:31:34 +1100

    move with navigation menu to tsx

---
+++ src/layouts/WithNavigationMenu.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useEffect } from "react";
+import { NavigationMenu } from "components/Navigation/NavigationMenu";
+import { HiHeart } from "react-icons/hi";
+import { setThemeColorMatchNavBar } from "utils/PageUtils";
+import { profile } from "data/profile";
+
+export type WithNavigationMenuProps = {
+  children: ReactNode;
+};
+
+export default function WithNavigationMenu({
+  children,
+}: WithNavigationMenuProps): JSX.Element {
+  useEffect(() => {
+    setThemeColorMatchNavBar();
+  }, []);
+
+  return (
+    <div>
+      <NavigationMenu />
+
+      <div className="pt-8">
+        {children}
+
+        <div className="text-center text-gray-500 my-4 text-sm">
+          Hand crafted with{" "}
+          <span className="text-red-400 text-xl">
+            <HiHeart />
+          </span>{" "}
+          by {profile.name}
+        </div>
+      </div>
+    </div>
+  );
+}
