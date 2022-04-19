f201ee60ffa1957fd540ed410e74c1e91881ad22
Ping Cheng <neroc7@gmail.com>
2022-04-19 22:48:03 +1000

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,31 @@
 module.exports = {
-    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                body: ["-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"],
-                code: ["source-code-pro", "Menlo", "Monaco", "Consolas", "Courier New", "monospace"]
-            }
-        },
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+        code: [
+          "source-code-pro",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Courier New",
+          "monospace",
+        ],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  plugins: [],
 };
