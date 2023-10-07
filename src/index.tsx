3bec01d602f7db94e2e3e511fd08cbff450899f6
Ping Cheng <neroc7@gmail.com>
2023-10-07 22:58:08 +1100

    update create root for react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./sass/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  throw new Error('Could not find root element with id "root"');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
